d385e101e1f1599e93cab838fb83edc1f5fc3c3b
harininishtala <harininishtala@gmail.com>
2025-02-09 01:42:16 +0530

    Add track and alert

---
+++ tracker/TrackAndAlert.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Management;
+using System.Windows.Forms;
+
+class TrackAndAlert{
+    static void Main(){
+        ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT BatteryStatus, EstimatedChargeRemaining FROM Win32_Battery");
+        foreach (ManagementObject queryObj in searcher.Get())
+        {
+            if((ushort)queryObj["EstimatedChargeRemaining"]<31)MessageBox.Show("Your battery is below 32!", "Please charge!");
+            else if((ushort)queryObj["EstimatedChargeRemaining"]>97)MessageBox.Show("Your battery is above 97!", "STOP!");
+        }
+    }
+}
